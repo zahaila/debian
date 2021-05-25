84729a45f8e71aa8786e500bfdbb88b9289754af
zaioll <zaioll@protonmail.com>
2021-05-25 15:30:38 -0400

    Remove repos inúteis

--- pre-config.sh
+++ pre-config.sh
@@ -3,8 +3,6 @@
 :>/etc/apt/sources.list
 echo "deb http://ftp.de.debian.org/debian/ stretch main contrib non-free">>/etc/apt/sources.list
 echo "deb http://ftp.de.debian.org/debian/ stretch-updates main contrib non-free">>/etc/apt/sources.list
-echo "deb http://sft.if.usp.br/debian/ stretch contrib non-free main">>/etc/apt/sources.list
-echo "deb-src http://sft.if.usp.br/debian/ stretch contrib non-free main">>/etc/apt/sources.list
 echo "deb http://deb.debian.org/debian/ stretch-updates main contrib">>/etc/apt/sources.list
 echo "deb-src http://deb.debian.org/debian/ stretch-updates main contrib">>/etc/apt/sources.list
 echo "deb http://security.debian.org/ stretch/updates main main contrib non-free">>/etc/apt/sources.list
